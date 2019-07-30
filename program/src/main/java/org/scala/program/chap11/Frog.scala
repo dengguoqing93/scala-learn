77602802de501bbd2b8ef5c09b9d444cac3ed744
dengguoqing <dengguoqing93@163.com>
2019-07-30 08:19:11 +0800

    概率论与数理统计

---
+++ program/src/main/java/org/scala/program/chap11/Frog.scala
@@ -0,0 +1,11 @@
+package org.scala.program.chap11
+
+/**
+  * 弗罗格
+  *
+  * @author dengguoqing
+  * @since 2019/7/27
+  */
+class Frog extends Philosophical {
+  override def toString: String = "green"
+}
---
+++ program/src/main/java/org/scala/program/chap11/Philosophical.scala
@@ -0,0 +1,13 @@
+package org.scala.program.chap11
+
+/**
+  *
+  * 哲学的
+  * @author dengguoqing
+  * @since 2019/7/27
+  */
+trait Philosophical {
+  def philosophize() = {
+    println("I consume memory,therefore I am")
+  }
+}
